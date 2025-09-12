44a131849a1c3209614a7ae869756f7fe9583a49
sean80 <kumtle80@gmail.com>
2025-09-12 12:42:30 +0900

    제미나이키 공용사용

--- src/geminiClient.js
+++ src/geminiClient.js
@@ -3,3 +3,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+export const genAI1 = new GoogleGenerativeAI(process.env.GEMINI_API_KEY1);
+export const genAI2 = new GoogleGenerativeAI(process.env.GEMINI_API_KEY2);
--- src/server.js
+++ src/server.js
@@ -1,22 +1,35 @@
 import express from "express";
 import { dbPool } from "./dbConfig.js";
-import { genAI } from "./geminiClient.js";
+import { genAI, genAI1, genAI2 } from "./geminiClient.js";
 import cors from "cors";
 
 const app = express();
 app.use(express.json());
 
+let idx = 0; // 라운드 로빈용 인덱
+
 app.use(
   cors({ origin: ["http://localhost:3000", "http://192.168.0.51:3000"] })
 );
 // DB → Gemini → 응답
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 app.post("/ask", async (req, res) => {
   try {
     const { question, filters } = req.body;
     const { indutyType, emdType } = filters || {};
-    console.log(indutyType);
+    // 라운드 로빈: idx 0 → 1 → 2 → 0 ...
+    if (idx > 2) idx = 0;
+    idx++;
+
+    let selectedGenAI;
+    if (idx === 1) selectedGenAI = genAI;
+    else if (idx === 2) selectedGenAI = genAI1;
+    else selectedGenAI = genAI2;
+
+    const model = selectedGenAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+    });
+
     // DB 조회 (프리페어드 스테이트먼트 사용)
     const [rows] = await dbPool.query(
       `SELECT * 
@@ -39,9 +52,10 @@ app.post("/ask", async (req, res) => {
     위 리스트를 기반으로 질문에 맞게 대답해주세요.
     `;
 
-    console.log(prompt);
-    const result = await model.generateContent(prompt);
-    const answer = result.response.text();
+    //console.log(prompt);
+    //const result = await model.generateContent(prompt);
+    //const answer = result.response.text();
+    const answer = `안녕하세요. 다음은 DB에서 불러온 가게 리스트입니다:${shopList}`;
 
     return res.status(200).json({ answer, rows });
   } catch (err) {
